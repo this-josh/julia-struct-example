a3e8778886f7cbde7a88a3878db3f25b0a9f4f36
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-03-30 14:07:24 +0100

    forgot to create new case of Foo

--- src/struct.jl
+++ src/struct.jl
@@ -8,5 +8,6 @@ using SparseArrays
     d::SparseArrays.SparseMatrixCSC{Int64,Int64}
     function Foo(a, b, c, d)
         d isa SparseArrays.SparseMatrixCSC{Int64,Int64} || throw(ArgumentError("`b` is not an Integer!"))
+        new(a, b, c, d)
     end
 end
